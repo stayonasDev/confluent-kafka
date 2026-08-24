6e8ab25ad6304d1bd4bbc476ef5bdba1b9a5f0b6
stayonasdev <stay_on_track@naver.com>
2026-08-24 14:31:50 +0900

    Create Kafka Consumer - offset_reset_config And poll in wake up

---
+++ consumers/src/main/java/com/example/kafka/ConsumerWakeup.java
@@ -0,0 +1,73 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class ConsumerWakeup {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerWakeup.class);
+
+    public static void main(String[] args) {
+
+        String topicName = "simple-topic";
+
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01");
+        //Default 마지막 offset
+//        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        //처음부터 읽지만 이미 __consumer_offsets에 있는 메시지면 처음부터 불가X
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(List.of(topicName));
+
+        //main thread
+        Thread mainThread = Thread.currentThread();
+
+        //메인 스레드가 죽기 전에 훅 이벤트
+        //Main Thread 종료 시 별도의 Thread로 KafkaConsumer wakeup() 메소드를 호출하게 함
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("main program starts to exit by calling wakeup");
+                //while 문에서 빠져나가기 위한 poll 기능에 wakeup (연결 종료를 위해)
+                consumer.wakeup();
+
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord record : consumerRecords) {
+                    log.info("record key:{}, record value:{}, partition:{}, record offset:{}",
+                            record.key(), record.value(), record.partition(), record.offset());
+
+                }
+            }
+        } catch (WakeupException e) {
+            log.error("wakeup exception has been called");
+        } finally {
+            log.info("finally consumer is closing");
+            consumer.close();
+        }
+
+    }
+}
---
+++ consumers/src/main/java/com/example/kafka/SimpleConsumer.java
@@ -0,0 +1,43 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+public class SimpleConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(SimpleConsumer.class);
+
+    public static void main(String[] args) {
+
+        String topicName = "simple-topic";
+
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(List.of(topicName));
+
+        while(true){
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(1000));
+            for(ConsumerRecord record : consumerRecords){
+                log.info("record key:{}, record value:{}, partition:{}",
+                        record.key(), record.value(), record.partition());
+
+            }
+        }
+
+    }
+}
